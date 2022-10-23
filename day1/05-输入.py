8df43f288cf8445369b0f2c6ade0677295fc04e2
fine <fine@fine.com>
2022-10-23 16:28:58 +0800

    数据类型

--- day1/05-输入.py
+++ day1/05-输入.py
@@ -3,8 +3,10 @@
 input('输入描述：')
 """
 
-name = input('姓名:')
-print(name)
-
 # 需求：
 # 接收用户输入 年龄，住址
+name = input('姓名:')
+age = input('年龄：')
+address = input('地址：')
+
+print(name, age, address)
---
+++ day1/06-数据类型.py
@@ -0,0 +1,33 @@
+# TODO 数字  int float
+num1 = 10
+num2 = 10.2
+print(type(num1))
+print(type(num2))
+print('==' * 20)
+
+# TODO 字符串
+# 以下这4种书写方式都是字符串
+# 以下这4种方式的区别
+"""
+1. 单引号和双引号没有区别
+2. 三单引号和三双引号没有区别
+3. 三引号会把字符串的格式输出到终端
+"""
+str1 = 'hello'
+str2 = "hello"
+str3 = '''hello'''
+str4 = """hello"""
+print(type(str1))
+print(type(str2))
+print(type(str3))
+print(type(str4))
+# 复制一行快捷键： ctrl d
+# print(str1)
+# print(str4)
+print('==' * 20)
+
+# TODO 布尔类型
+bool1 = True
+bool2 = False
+print(type(bool1))
+print(type(bool2))
