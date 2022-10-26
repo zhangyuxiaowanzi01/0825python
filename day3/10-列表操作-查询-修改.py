4f04420becd31b31ba379ebb79862d3cc44734b5
fine <fine@fine.com>
2022-10-26 16:47:52 +0800

    列表操作

---
+++ day3/10-列表操作-查询-修改.py
@@ -0,0 +1,20 @@
+# 查询
+# list[索引] 通过索引获取元素
+list1 = ['python', 'mysql', 'git']
+print(list1[1])
+print(list1[-2])
+
+# list.index(数据) 查询数据第一次出现的索引，查询不到抛出错误
+list1 = ['python', 'mysql', 'git', 'mysql']
+print(list1.index('mysql'))
+# list1.index('linux')  # 询不到抛出错误
+
+# len() 通用方法，用来查询容器数据的长度
+print(len(list1))
+print(len('hello'))
+
+
+# 修改
+# 语法：list[下标] = value
+list1[0] = 'java'
+print(list1)
---
+++ day3/11-查询操作-排序.py
@@ -0,0 +1,16 @@
+
+list1 = ['g', 'd', 'f', 'a', 'z']
+# 列表.sort()  升序排序 从小到大
+list1.sort()
+print(list1)
+
+# 列表.sort(reverse=True) 降序排序 从大到小
+list1.sort(reverse=True)
+print(list1)
+
+# list.reverse()
+list1.reverse()
+print(list1)
+
+
+
