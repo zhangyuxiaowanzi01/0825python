c6125394c3d796c6c95aec5338ec578f7084d7de
fine <fine@fine.com>
2022-10-27 12:00:02 +0800

    列表嵌套

--- day4/04-列表嵌套.py
+++ day4/04-列表嵌套.py
@@ -5,3 +5,18 @@ list1 = [
 
 # 需求
 # 把这个列表中的所有元素都遍历出来
+# TODO while
+i = 0
+while i < len(list1):
+    j = 0
+    item_list = list1[i]  # 小列表
+    while j < len(item_list):
+        print(item_list[j])
+        j += 1
+    i += 1
+print('==' * 20)
+
+# TODO for
+for item_list in list1:
+    for item in item_list:
+        print(item)
