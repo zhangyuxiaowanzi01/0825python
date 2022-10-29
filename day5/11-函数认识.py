9b32758edb4130f72907e2fe30a1355d3f5c2d4b
fine <fine@fine.com>
2022-10-29 16:29:26 +0800

    函数认识和使用

---
+++ day5/11-函数认识.py
@@ -0,0 +1,32 @@
+"""
+系统函数
+print()   输出内容
+len()     获取容器长度
+min()     获取容器中最小值
+max()     获取容器中最大值
+
+函数
+带名字的代码块，封装了一个特定的功能。
+
+自定义函数
+自己编写的函数
+"""
+
+# 变量
+a = "AAAAAAAAAAAAAAAAAAAAAAAAA"
+
+# for j in range(3):
+#     for i in range(3):
+#         print(a)
+
+for i in range(300):
+    print(a)
+print(111111)
+
+for i in range(300):
+    print(a)
+print(22222)
+
+for i in range(300):
+    print(a)
+print(33333)
