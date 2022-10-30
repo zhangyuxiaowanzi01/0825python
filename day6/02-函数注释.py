21851aac5a3004c14b23b5a4098267c736eba214
fine <fine@fine.com>
2022-10-30 11:35:08 +0800

    参数加强

---
+++ day6/02-函数注释.py
@@ -0,0 +1,26 @@
+"""
+函数注释（函数说明文档）
+"""
+
+
+# def fn():
+#     """
+#     函数注释
+#     """
+
+# help(fn)  # 查看函数的说明文档
+
+
+def sum(a, b):
+    """
+    求2个数的和
+    @param a: 数字1
+    @param b: 数字2
+    return 2个数的和
+    """
+    return a + b
+
+
+help(sum)
+
+help(print)
---
+++ day6/03-参数加强.py
@@ -0,0 +1,41 @@
+"""
+# 定义函数-参数
+# 必填参数（必需参数）
+    # 必须要传入对应的实参
+# 默认参数
+    # 带有默认值的参数就叫做默认参数
+# 说明：
+    必填参数必须写在默认参数的前面
+"""
+
+
+# TODO 默认参数
+# class_name:默认参数
+def fn2(name, age, class_name='0825'):
+    """
+    @param name: 姓名
+    @param age: 年龄
+    @param class_name: 班级
+    @return:
+    """
+    print(name, age, class_name)
+
+
+fn2(1, 2)
+
+
+# TODO 必填参数
+def fn1(a, b):
+    print(a, b)
+
+
+"""
+# 调用函数-参数
+位置参数：按照形参的位置传入参数
+关键字参数：按照形参名字传递参数
+"""
+# TODO 位置参数
+fn1(2, 1)
+
+# TODO 关键字参数
+fn1(b=100, a=200)
