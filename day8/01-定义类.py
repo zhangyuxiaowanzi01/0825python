066a756a5e2c43c572f0e6ead712dee495b4112f
fine <fine@fine.com>
2022-11-02 10:31:10 +0800

    定义类创建对象

---
+++ day8/01-定义类.py
@@ -0,0 +1,11 @@
+"""
+class 类名:
+    pass
+"""
+
+# 定义类
+class Person:
+    pass
+
+
+print(Person)
---
+++ day8/02-创建对象.py
@@ -0,0 +1,16 @@
+"""
+类名()
+"""
+
+
+class Person:
+    pass
+
+
+# 创建对象
+# p1:Person类的对象， Person类的实例
+# 说明：一个类可以创建多个对象,没创建一个对象就开辟新的内存空间进行存储
+p1 = Person()
+p2 = Person()
+print(p1)
+print(p2)
