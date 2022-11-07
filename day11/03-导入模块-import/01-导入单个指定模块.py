1dff43aff43e5796767581938a148f77f51e32be
fine <fine@fine.com>
2022-11-07 11:47:53 +0800

    导入单个指定模块

---
+++ day11/03-导入模块-import/01-导入单个指定模块.py
@@ -0,0 +1,18 @@
+"""
+# 导入单个指定模块
+import 模块名
+说明：模块名就是没有后缀的文件名字
+导入模块模块的目的：导入模块之后可以使用模块中的资源
+资源：变量，函数，类
+"""
+# 导入module_a
+import module_a
+
+print(module_a.name)
+
+module_a.fn()
+
+print(module_a.Demo())
+
+
+
---
+++ day11/03-导入模块-import/module_a.py
@@ -0,0 +1,12 @@
+# 变量
+name = 'module_a'
+
+
+# 函数
+def fn():
+    print('module_a fn函数')
+
+
+# 类
+class Demo:
+    pass
