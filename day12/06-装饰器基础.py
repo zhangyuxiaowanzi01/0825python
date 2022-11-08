5e6087e71e5e6b7656479f5581dddd59bd1077fe
fine <fine@fine.com>
2022-11-08 14:45:41 +0800

    装饰器基础

---
+++ day12/06-装饰器基础.py
@@ -0,0 +1,29 @@
+def fn2():
+    # 开始时间
+    print('fn2')
+    # 结束时间
+    # 结果 = 结束时间 - 开始时间
+
+
+# 定义装饰器
+# fn: 被装饰的函数
+def wrapper(fn):
+    def inner():
+        print('前置扩展')
+        fn()
+        print('后置扩展')
+
+    return inner
+
+
+# 使用装饰器
+def fn1():
+    print('fn1')
+
+
+fn1 = wrapper(fn1)
+fn1()
+
+# fn2 = wrapper(fn2)
+# fn2()
+
