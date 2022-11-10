0b824b4a1fd373eeadd174242675aedd225a4941
fine <fine@fine.com>
2022-11-10 15:30:20 +0800

    改变目录名字

--- day1/03-输出.py
+++
@@ -1,9 +0,0 @@
-"""
-语法：
-print([content1, content2, content3, ...], end='\n', sep=' ')
-content:参数
-end:print的结尾符号，默认值是'\n'
-sep:输出多个字符之间的间隔符号，默认值是空格
-"""
-print('hello-world', 'hello-python', 'hello-mysql', end=' ===== ', sep='|')
-print('hello-git')
