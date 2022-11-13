31627504e91e448e966a0fa9e7ef8a23ff986fa8
fine <fine@fine.com>
2022-11-13 15:44:30 +0800

    列子查询

--- mysql/day01/advanced/09-子查询.sql
+++ mysql/day01/advanced/09-子查询.sql
@@ -7,3 +7,22 @@ SELECT * FROM student WHERE class_id = '1';
 -- 子查询语句
 SELECT * FROM student WHERE class_id = 
 (SELECT class_id FROM class WHERE class_name='1班');
+
+-- 列子查询
+-- 查询1班和2班所有学生信息
+-- 1. 查询1班和2班的class_id
+SELECT class_id FROM class WHERE class_name = '1班' OR class_name = '2班';
+SELECT class_id FROM class WHERE class_name in ('1班', '2班');
+-- 2. 利用classid查询对应学生
+SELECT * FROM student WHERE class_id = 1 OR class_id=2;
+SELECT * FROM student WHERE class_id in (1, 2);
+-- 子查询语句
+SELECT * FROM student WHERE class_id 
+in (SELECT class_id FROM class WHERE class_name in ('1班', '2班'));
+
+
+
+
+
+
+
