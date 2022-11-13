8251a9d1a2d7dd7638ba65badb305a949b594762
fine <fine@fine.com>
2022-11-13 11:52:51 +0800

    分组查询

--- mysql/day01/advanced/05-分组查询.sql
+++ mysql/day01/advanced/05-分组查询.sql
@@ -17,4 +17,14 @@ SELECT class_id, GROUP_CONCAT(stu_name) stu_names FROM student GROUP BY class_id
 -- 查询每个班年龄大于30的所有同学姓名
 SELECT class_id, GROUP_CONCAT(stu_name) FROM student WHERE stu_age > 30 GROUP BY class_id;
 
+-- HAVING 配合 GROUP BY使用
+-- 查询每个年龄人数大于1的年龄和人数
+SELECT stu_age, COUNT(*) FROM student GROUP BY stu_age;
+SELECT stu_age, COUNT(*) FROM student GROUP BY stu_age HAVING COUNT(*) > 1;
+
+-- 查询班级人数大于3的班级
+SELECT class_id, COUNT(*) FROM student GROUP BY class_id HAVING COUNT(*) > 3;
+
+-- 平均年龄大于30的班级
+SELECT class_id, AVG(stu_age) FROM student GROUP BY class_id HAVING AVG(stu_age) > 30;
 
