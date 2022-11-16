8001f9e2cea4bc0dfcda4081f2ce2f4340ac8cde
fine <fine@fine.com>
2022-11-16 11:40:25 +0800

    db类

---
+++ mysql/day03/03-三元运算符.py
@@ -0,0 +1,9 @@
+if 1 > 2:
+    print(1)
+else:
+    print(2)
+
+# 三元运算符（表达式）
+# 语法： 表达式 if 条件表达式 else 表达式
+num = 1 if 2 > 1 else 2
+print(num)
---
+++ mysql/day03/db.py
@@ -0,0 +1,48 @@
+"""
+数据库操作类
+类名：Database
+方法：
+    init:
+        conn
+        cursor
+    read
+    write
+    del:
+        cursor.close()
+        conn.close()
+"""
+import pymysql
+
+
+class Database:
+    def __init__(self, database, host='localhost',
+                 port=3306, user='root', password='root', cursor=pymysql.cursors.Cursor):
+        self.conn = pymysql.connect(host=host, port=port,
+                                    user=user, password=password,
+                                    database=database, charset='utf8')
+        self.cursor = self.conn.cursor(cursor=cursor)
+        self.cursor_type = () if cursor == pymysql.cursors.Cursor else []
+
+    def read(self, sql, args=None):
+        """读操作"""
+        try:
+            rows = self.cursor.execute(sql, args)
+        except Exception as e:
+            print(e)
+            return 0, self.cursor_type
+        else:
+            all_data = self.cursor.fetchall()
+            return rows, all_data
+
+    def write(self):
+        """写操作"""
+        pass
+
+
+if __name__ == '__main__':
+    database = Database('advanced')
+    # database.read()  # rows data
+    rows, data = database.read('select * from news where itle=%s', ['title1'])
+
+    print(rows)
+    print(data)
