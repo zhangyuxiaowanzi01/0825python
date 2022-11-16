fb3fd583a8d953f5c9e6455901dc8422fa89274c
fine <fine@fine.com>
2022-11-16 12:00:12 +0800

    db类

--- mysql/day03/db.py
+++ mysql/day03/db.py
@@ -35,15 +35,27 @@ class Database:
             all_data = self.cursor.fetchall()
             return rows, all_data
 
-    def write(self):
+    def write(self, sql, args=None):
         """写操作"""
-        pass
+        try:
+            rows = self.cursor.execute(sql, args)
+        except Exception as e:
+            print(e)
+            self.conn.rollback()
+            return 0
+        else:
+            self.conn.commit()
+            return rows
 
+    def __del__(self):
+        self.cursor.close()
+        self.conn.close()
 
 if __name__ == '__main__':
     db = Database('advanced')
     # database.read()  # rows data
-    rows, data = db.read('select * from news where itle=%s', ['title1'])
+    # rows, data = db.read('select * from news where itle=%s', ['title1'])
 
-    print(rows)
-    print(data)
+    # 删除操作
+    print(db.write('delete from new where title = %s', ['title4']))
+
